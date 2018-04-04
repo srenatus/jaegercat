678b4d71bd9a6349114d2a65f019509a9e910edb
Stephan Renatus <srenatus@chef.io>
2018-04-04 08:32:01 +0200

    Add --sample-services command, exposing remote sampling strategy endpoint
    This way, with an invocation like
    
        ./jaegercat --sample-services foo-service,bar-service
    
    the utility will expose an HTTP endpoint on 127.0.0.1:5778, and when asked
    for a service's sample strategy via GET /sampling?service=foo-service, it
    will respond with a hard-coded "output all samples" setting.
    
    The mentioned endpoint is queried in that way by the jager-go library every
    minute, so it takes moment (worst case: the whole minute) to have the
    clients pick up that they are now supposed to output all their spans.
    
    This can be built for Linux locally using docker:
    
        $ docker run -v $(pwd):/src --workdir /src -it rust:1.25.0 bash
        # rustup target add x86_64-unknown-linux-musl
        # cargo build --target x86_64-unknown-linux-musl
    
    and then copied from (outside of the docker container)
    
        target/x86_64-unknown-linux-musl/debug/jaegercat
    
    Signed-off-by: Stephan Renatus <srenatus@chef.io>

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,32 @@ extern crate slog;
 extern crate sloggers;
 #[macro_use]
 extern crate trackable;
+extern crate futures;
+extern crate hyper;
+extern crate url;
 
 use std::io::{self, Write};
 use std::net::{SocketAddr, UdpSocket};
 use std::thread;
+use std::collections::{HashMap, HashSet};
 use clap::{App, Arg};
 use jaegercat::thrift::{EmitBatchNotification, Protocol};
 use sloggers::Build;
 use sloggers::terminal::{Destination, TerminalLoggerBuilder};
 use sloggers::types::SourceLocation;
 use trackable::error::Failure;
+use futures::future::Future;
+
+use hyper::header::ContentLength;
+use hyper::server::{Http, Request, Response, Service};
+use hyper::{Method, StatusCode};
+use url::form_urlencoded;
 
 macro_rules! try_parse {
     ($expr:expr) => { track_try_unwrap!($expr.parse().map_err(Failure::from_error)) }
 }
+
+static SAMPLE_ALL_RESP: &'static str = r#"{"strategyType": "PROBABILISTIC", "probabilisticSampling": {"samplingRate": 1}}"#;
 
 fn main() {
     let matches = App::new("jaegercat")
@@ -59,6 +71,13 @@ fn main() {
                 .default_value("info")
                 .possible_values(&["debug", "info", "error"]),
         )
+        .arg(
+            Arg::with_name("SAMPLE_SERVICES")
+                .short("S")
+                .long("sample-services")
+                .use_delimiter(true)
+                .takes_value(true),
+        )
         .get_matches();
 
     let compact_thrift_port: u16 = try_parse!(matches.value_of("COMPACT_THRIFT_PORT").unwrap());
@@ -78,6 +97,7 @@ fn main() {
             .level(log_level)
             .build()
     );
+    let svcs = matches.values_of("SAMPLE_SERVICES").unwrap_or_default().map(String::from).collect::<HashSet<String>>();
 
     let mut threads = Vec::new();
     for (port, protocol) in [
@@ -132,8 +152,65 @@ fn main() {
         });
         threads.push(thread);
     }
+
+    // agent sampling handler
+    let thread = thread::spawn(move || {
+
+        let addr = "127.0.0.1:5778".parse().unwrap();
+        let svc = SamplingService{
+            logger: logger,
+            enabled_services: svcs
+        };
+
+        let server = Http::new().bind(&addr, move || Ok(svc.clone())).unwrap();
+        server.run().unwrap();
+    });
+    threads.push(thread);
+
     for t in threads {
         let _ = t.join();
+    }
+}
+
+#[derive(Clone)]
+struct SamplingService {
+    logger: slog::Logger,
+    enabled_services: HashSet<String>,
+}
+
+impl Service for SamplingService {
+    // boilerplate hooking up hyper's server types
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    // The future representing the eventual Response your call will
+    // resolve to. This can change to whatever Future you need.
+    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
+
+    fn call(&self, req: Request) -> Self::Future {
+        let response = match (req.method(), req.path()) {
+            (&Method::Get, "/sampling") => {
+                let query = match req.uri().query() {
+                    Some(q) => q,
+                    None => "",
+                };
+                let params = form_urlencoded::parse(query.as_ref()).into_owned().collect::<HashMap<String, String>>();
+                let svc = match params.get("service") {
+                    Some(n) => n,
+                    None => "unknown",
+                };
+                if self.enabled_services.contains(svc) {
+                    info!(self.logger, "enabling service {:?}", svc);
+                    Response::new()
+                        .with_header(ContentLength(SAMPLE_ALL_RESP.len() as u64))
+                        .with_body(SAMPLE_ALL_RESP)
+                } else {
+                    Response::new().with_status(StatusCode::NotFound)
+                }
+            },
+            _ => Response::new().with_status(StatusCode::NotFound)
+        };
+        Box::new(futures::future::ok(response))
     }
 }
 
